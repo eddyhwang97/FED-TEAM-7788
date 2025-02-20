711dddea0c08dec2d79712ce38ffc50d40e8c9d1
kimeming <kimeming030@gmail.com>
2025-02-20 15:52:21 +0900

    url 카피 코드 오류수정

--- js/base.js
+++ js/base.js
@@ -1,77 +1,92 @@
 $(() => {
-  const $linkCopyBtn = $(".link-copy > button");
-
-  $linkCopyBtn.on("click", function () {
-    if (!$(this).hasClass("active")) {
-      $(this).addClass("active");
-      $(".url-copy").addClass("on");
-    } else {
-      $(this).removeClass("active");
-      $(".url-copy").removeClass("on");
-    }
-  });
-
-
-  window.fn_copyUrl = function () {
-    $("#urlCopy").select();
-    document.execCommand("copy");
-    alert("URL 주소가 복사 되었습니다.");
-    return false;
-  };
-
-  $(document).mouseup(function (e) {
-    if (
-      $linkCopyBtn.has(e.target).length === 0 &&
-      !$(e.target).is(".url-copy")
-    ) {
-      $linkCopyBtn.removeClass("active");
-      $(".url-copy").removeClass("on");
-    }
-  });
-
-  window.fn_print = function () {
-    let $container = $(".sub-container").clone();
-
-    let cssText = ""; // 스타일 복사
-    $("link").each(function (index, node) {
-      cssText += '<link rel="stylesheet" href="' + $(node).attr("href") + '" >';
+    const $linkCopyBtn = $(".link-copy > button");
+  
+    $linkCopyBtn.on("click", function () {
+      if (!$(this).hasClass("active")) {
+        $(this).addClass("active");
+        $(".url-copy").addClass("on");
+      } else {
+        $(this).removeClass("active");
+        $(".url-copy").removeClass("on");
+      }
     });
-    
-
-    let innerHtml = $container[0].innerHTML;
-    let popupWindow = window.open("", "_blank", "width=700,height=800");
-    popupWindow.document.write(
-      "<!DOCTYPE html>" +
-        "<html>" +
-        "<head>" +
-        cssText +
-        "</head>" +
-        "<body>" +
-        innerHtml +
-        "</body>" +
-        "</html>"
-    );
-    popupWindow.document.close();
-    popupWindow.focus();
-
-    popupWindow.onafterprint = function () {
-      popupWindow.close();
+  
+    window.fn_copyUrl = function () {
+      const copyUrl = document.getElementById("urlCopy");
+      if (!copyUrl) {
+        alert("복사할 URL이 없습니다.");
+        return false;
+      }
+  
+      copyUrl.select();
+      copyUrl.setSelectionRange(0, 99999); // 모바일 대응
+  
+      navigator.clipboard.writeText(copyUrl.value)
+        .then(() => {
+          alert("URL 주소가 복사 되었습니다.");
+        })
+        .catch(() => {
+          alert("URL 복사에 실패했습니다.");
+        });
+  
+      return false;
     };
-
-    popupWindow.onload = function () {
-      popupWindow.print();
+  
+    $(document).mouseup(function (e) {
+      if (
+        $linkCopyBtn.has(e.target).length === 0 &&
+        !$(e.target).is(".url-copy")
+      ) {
+        $linkCopyBtn.removeClass("active");
+        $(".url-copy").removeClass("on");
+      }
+    });
+  
+    window.fn_print = function () {
+      let $container = $(".sub-container").clone();
+  
+      let cssText = "";
+      $("link").each(function (index, node) {
+        cssText += '<link rel="stylesheet" href="' + $(node).attr("href") + '" >';
+      });
+  
+      let innerHtml = $container[0].innerHTML;
+      let popupWindow = window.open("", "_blank", "width=700,height=800");
+      popupWindow.document.write(
+        "<!DOCTYPE html>" +
+          "<html>" +
+          "<head>" +
+          cssText +
+          "</head>" +
+          "<body>" +
+          innerHtml +
+          "</body>" +
+          "</html>"
+      );
+      popupWindow.document.close();
+      popupWindow.focus();
+  
+      popupWindow.onafterprint = function () {
+        popupWindow.close();
+      };
+  
+      popupWindow.onload = function () {
+        popupWindow.print();
+      };
     };
-  };
-});
-
-function getInputValue() {
+  });
+  
+  function getInputValue() {
     const copyUrl = document.getElementById("urlCopy");
-    let url = window.document.location.href;
-    copyUrl.setAttribute("value", url);
+    if (copyUrl) {
+      let url = window.location.href;
+      copyUrl.value = url; // setAttribute 대신 직접 value 설정
+    }
   }
-
+  
   $(window).on("load", function () {
     if ($(".url-copy").length > 0) {
       getInputValue();
     }
   });
+  
