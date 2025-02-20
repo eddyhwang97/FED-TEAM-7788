57b1846c7c51c2fa9322c4eb0af4757490375036
eddyhwang97 <hwangdw1997@gmail.com>
2025-02-20 15:53:20 +0900

    .

--- js/community.js
+++ js/community.js
@@ -1,12 +1,9 @@
 // community JS - community.js
 import communityData from '../data/community_data.json' with {type:'json'};
-import CommunityFn from "./function/community_Fn.js";
+import { ChangeTabContent, ToggleActiveFn, ParentToggleActiveFn } from "./function/community_Fn.js";
 // console.log(communityData);
 // set communityData in LocalStorage
 localStorage.setItem("community_data", JSON.stringify(communityData));
-
-// set communityFn
-const cmFn = new CommunityFn();
 
 // 대상
 // tab-content map
@@ -14,11 +11,13 @@ const targetTab1 = document.querySelector("#announcement-tab>ul");
 const targetTab2 = document.querySelector("#faq-tab>ul");
 const targetTab3 = document.querySelector("#freeboard-tab>ul");
 
-
 const tabContent1 = `
-  ${communityData.filter(x => x.type === 'announcement').sort((a,b)=>a.date==b.date?0:a.date>b.date?-1:1)
-    .map((v, i) =>
-      `
+  ${communityData
+    .filter((x) => x.type === "announcement")
+    .sort((a, b) => (a.date == b.date ? 0 : a.date > b.date ? -1 : 1))
+    .map(
+      (v, i) =>
+        `
     <li class="list">
       <div class="list-header">
         <p class="list-num">${i + 1}</p>
@@ -36,9 +35,12 @@ const tabContent1 = `
     .join("")}
   `;
 const tabContent2 = `
-  ${communityData.filter(x => x.type === 'faq').sort((a,b)=>a.date==b.date?0:a.date>b.date?-1:1)
-    .map((v, i) =>
-      `
+  ${communityData
+    .filter((x) => x.type === "faq")
+    .sort((a, b) => (a.date == b.date ? 0 : a.date > b.date ? -1 : 1))
+    .map(
+      (v, i) =>
+        `
     <li class="list">
       <div class="list-header">
         <p class="list-num">${i + 1}</p>
@@ -56,9 +58,12 @@ const tabContent2 = `
     .join("")}
   `;
 const tabContent3 = `
-  ${communityData.filter(x => x.type === 'freeboard').sort((a,b)=>a.date==b.date?0:a.date>b.date?-1:1)
-    .map((v, i) =>
-      `
+  ${communityData
+    .filter((x) => x.type === "freeboard")
+    .sort((a, b) => (a.date == b.date ? 0 : a.date > b.date ? -1 : 1))
+    .map(
+      (v, i) =>
+        `
     <li class="list">
       <div class="list-header">
         <p class="list-num">${i + 1}</p>
@@ -76,16 +81,15 @@ const tabContent3 = `
     .join("")}
   `;
 
-  // start Fn
-  window.onload = () => {
-    
-    targetTab1.innerHTML = tabContent1;
-    targetTab2.innerHTML = tabContent2;
-    targetTab3.innerHTML = tabContent3;
-    const list = document.querySelectorAll(".list");
-    const writeBtn = document.querySelector('.write-btn')
+// start Fn
+window.onload = () => {
+  targetTab1.innerHTML = tabContent1;
+  targetTab2.innerHTML = tabContent2;
+  targetTab3.innerHTML = tabContent3;
+  const list = document.querySelectorAll(".list");
+  const writeBtn = document.querySelector(".write-btn");
 
-  cmFn.ChangeTabContent();
-  cmFn.ToggleActiveFn(list);
-  cmFn.ParentToggleActiveFn(writeBtn);
+  ChangeTabContent();
+  ToggleActiveFn(list);
+  ParentToggleActiveFn(writeBtn);
 };
--- js/function/community_Fn.js
+++ js/function/community_Fn.js
@@ -1,47 +1,44 @@
 // community_fn JS
-export default function CommunityFn() {
-  // ChangeTabContent s
-  this.ChangeTabContent = function () {
-    const tabs = document.querySelectorAll("[data-tab-target]");
-    const tabcon = document.querySelectorAll("[data-tab-content]");
-    tabs.forEach((tab) => {
-      tab.addEventListener("click", () => {
-        const target = document.querySelector(tab.dataset.tabTarget);
-        tabs.forEach((tab_all) => {
-          tab_all.classList.remove("active");
-        });
-        tabcon.forEach((tabc_all) => {
-          tabc_all.classList.remove("active");
-        });
-        target.classList.add("active");
-        tab.classList.add("active");
+
+// ChangeTabContent s
+function ChangeTabContent() {
+  const tabs = document.querySelectorAll("[data-tab-target]");
+  const tabcon = document.querySelectorAll("[data-tab-content]");
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+      const target = document.querySelector(tab.dataset.tabTarget);
+      tabs.forEach((tab_all) => {
+        tab_all.classList.remove("active");
+      });
+      tabcon.forEach((tabc_all) => {
+        tabc_all.classList.remove("active");
+      });
+      target.classList.add("active");
+      tab.classList.add("active");
+    });
+  });
+}
+//  ChangeTabContent e
+
+// ToggleActiveFn s
+function ToggleActiveFn(target) {
+  if (target.length === 1) {
+    target.addEventListener("click", () => {
+      target.classList.toggle("active");
+    });
+  } else if (target.length > 1) {
+    target.forEach((t) => {
+      t.addEventListener("click", () => {
+        t.classList.toggle("active");
       });
     });
-  };
-  //  ChangeTabContent e
-
-  // ToggleActiveFn s
-  this.ToggleActiveFn = function(target){
-    if(target.length ===1){
-      target.addEventListener("click",()=>{
-      target.classList.toggle("active");
-    })
-    }
-    else if(target.length >1){
-      target.forEach((t)=>{
-        t.addEventListener("click",()=>{
-        t.classList.toggle("active");
-      })
-      })
-    }
-    
-  }
-  // ParentToggleActiveFn s
-  this.ParentToggleActiveFn = function(target){
-    target.addEventListener("click",()=>{
-      target.parentElement.classList.toggle("active");
-    })
   }
 }
+// ParentToggleActiveFn s
+function ParentToggleActiveFn(target) {
+  target.addEventListener("click", () => {
+    target.parentElement.classList.toggle("active");
+  });
+}
 
-
+export { ChangeTabContent, ToggleActiveFn, ParentToggleActiveFn };
