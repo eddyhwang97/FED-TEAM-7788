ac115de99f358f1fadbd36b5258c8fc0709418f8
kimeming <kimeming030@gmail.com>
2025-02-25 11:22:10 +0900

    원복~~~

--- project/src/components/layout/FooterArea.jsx
+++ project/src/components/layout/FooterArea.jsx
@@ -4,6 +4,67 @@ import React from "react";
 
 export default function FooterArea(props) {
   return (
-    <></>
+    <footer className="footer">
+      <div className="inner">
+        <div className="footer-inner">
+          <div className="left">
+            <ul className="footer-link">
+              <li>
+                <a href="#">개인정보처리방침</a>
+              </li>
+              <li>
+                <a href="#">저작권보호정책</a>
+              </li>
+              <li>
+                <a href="#">이메일무단수집거부</a>
+              </li>
+              <li>
+                <a href="#">자주묻는질문</a>
+              </li>
+            </ul>
+          </div>
+          <div className="right">
+            <div className="family-site">
+              <button type="button" className="family-site-btn">
+                유관기관 바로가기<span className="blind">열림</span>
+              </button>
+              <ul className="site-list">
+                <li>
+                  <a href="#" target="_blank" title="새창열림">
+                    아무사이트나넣기
+                  </a>
+                </li>
+                <li>
+                  <a href="#" target="_blank" title="새창열림">
+                    아무사이트나넣기
+                  </a>
+                </li>
+                <li>
+                  <a href="#" target="_blank" title="새창열림">
+                    아무사이트나넣기
+                  </a>
+                </li>
+                <li>
+                  <a href="#" target="_blank" title="새창열림">
+                    아무사이트나넣기
+                  </a>
+                </li>
+                <li>
+                  <a href="#" target="_blank" title="새창열림">
+                    아무사이트나넣기
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="footer-btm">
+          <div className="footer-logo">
+            <img src="../img/common/logo-temp.svg" alt="하단 로고" />
+          </div>
+          <p className="copyright">ⓒ 2025. ChkChkBookBook All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
   );
 }
--- project/src/components/layout/TopArea.jsx
+++ project/src/components/layout/TopArea.jsx
@@ -5,8 +5,64 @@ import "../../css/common/_core.scss";
 import { Link } from "react-router-dom";
 import Gnb from "../module/Gnb";
 import { menu2 } from "../../js/data/gnb_data";
-import * as layoutFn from "../../js/function/layout";
+// import * as layoutFn from "../../js/function/layout";
 
 export default function TopArea() {
-  return <></>;
+  return (
+    <>
+      <div className="dimm"></div>
+      <div className="search-dimm"></div>
+      {/* <!-- header s --> */}
+      <header className="header">
+        <div className="header-top">
+          <ul className="link-list">
+            {menu2.map((v,i)=>(
+
+            <li key={i}>
+              <Link to={v.link}>
+                <span className={"icon"+v.class}>{v.txt}</span>
+              </Link>
+            </li>
+            ))}
+          </ul>
+        </div>
+        <div className="inner-header-wrap">
+          <div className="inner-header">
+            <h1 className="logo">
+              <Link to="/">
+                <img src="../img/common/logo-temp.svg" alt="로고" />
+              </Link>
+            </h1>
+            <Gnb />
+            <div className="header-util">
+              <button type="button" className="total-search-btn">
+                <span className="blind">통합검색 버튼</span>
+              </button>
+              <button type="button" className="search-close-btn">
+                <span className="blind">통합검색 닫기 버튼</span>
+              </button>
+              <button type="button" className="menu-btn">
+                <span className="blind">메뉴 버튼</span>
+              </button>
+            </div>
+            <div className="pc-header-util">
+              <button type="button" className="total-search-btn"></button>
+              <button type="button" className="search-close-btn"></button>
+            </div>
+          </div>
+        </div>
+        <div className="search-wrap">
+          <div className="search-form">
+            <form action="search.php">
+              <fieldset>
+                <legend className="blind">도서 통합검색 폼</legend>
+                <input type="text" id="mainSearchText" name="searchTxt" title="검색어" placeholder="찾으시는 도서가 있나요?" />
+                <button type="button" className="total-search-btn"></button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
