e1219b0acdb9b02a317b2fc00b7228008f4d8d7f
kimeming <kimeming030@gmail.com>
2025-02-27 12:18:03 +0900

    sub-top 컴포넌트 생성

--- project/src/components/module/SubTop.jsx
+++ project/src/components/module/SubTop.jsx
@@ -1,8 +1,43 @@
 // SubTop 컴포넌트 - SubTop.jsx
 import React from "react";
+// import { Link } from "react-router-dom";
+// import {menu} from "../../js/data/gnb_data.js";
 
 function SubTop(props) {
-  return <></>;
+  return (
+    <>
+    <div className="sub-top">
+       <ul className="breadcrumb">
+         <li className="home">
+           <a href="/"><span className="icon home"></span></a>
+         </li>
+         <li className="1depth"><a href="#">원뎁스</a></li>
+         <li className="2depth"><a href="#">투뎁스</a></li>
+       </ul>
+       <div className="title-box">
+         <h3 className="sub-title">페이지 서브 타이틀이 들어갑니다.</h3>
+         <ul className="sub-link">
+           <li className="link-copy">
+             <button type="button">
+               <img src="../img/common/icon-link.svg" alt="링크 공유 버튼" />
+             </button>
+             <div className="url-copy">
+               <input type="text" id="urlCopy" value="" />
+               <label for="urlCopy"></label>
+               <button type="button" onclick="fn_copyUrl();">복사</button>
+             </div>
+           </li>
+           <li className="link-print">
+             <button type="button" onclick="fn_print()">
+               <img src="../img/common/icon-print.svg" alt="페이지 프린트 버튼" />
+             </button>
+           </li>
+         </ul>
+       </div>
+     </div>
+    </>
+  )
+  
 }
 
 export default SubTop;
