78a6a8ee1b2541deefa9cafc053b4dd29523c0e0
kimeming <kimeming030@gmail.com>
2025-02-27 13:16:33 +0900

    서브 최종수정완료

--- project/src/components/layout/SubArea.jsx
+++ project/src/components/layout/SubArea.jsx
@@ -3,10 +3,9 @@
 import { Outlet } from "react-router-dom";
 import SubTop from "../module/SubTop";
 
-export default function SubArea({gnb1,gnb2}) {
+export default function SubArea() {
   return (
     <main className="sub-container" id="main">
-      <SubTop gnb1={gnb1} gnb2={gnb2} />
       <Outlet/>
     </main>
   );
