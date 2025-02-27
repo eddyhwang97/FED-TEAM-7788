de77302fcd9392ddfd3038e758ea352b13c5c0b8
kimeming <kimeming030@gmail.com>
2025-02-27 14:29:54 +0900

    원복

--- project/src/components/layout/SubArea.jsx
+++ project/src/components/layout/SubArea.jsx
@@ -1,9 +1,8 @@
 // SubArea 컴포넌트 - SubArea.jsx
 
 import { Outlet } from "react-router-dom";
-import SubTop from "../module/SubTop";
 
-export default function SubArea({ gnb1, gnb2 }) {
+export default function SubArea() {
   return (
     <main className="sub-container" id="main">
       <Outlet />
