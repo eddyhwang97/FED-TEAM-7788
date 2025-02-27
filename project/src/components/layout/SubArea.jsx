d414b8a5ee4ee9605c679cf1bc64a7fbd30cd643
eddyhwang97 <hwangdw1997@gmail.com>
2025-02-27 13:21:53 +0900

    서브탑 삭제

--- project/src/components/layout/SubArea.jsx
+++ project/src/components/layout/SubArea.jsx
@@ -3,12 +3,10 @@
 import { Outlet } from "react-router-dom";
 import SubTop from "../module/SubTop";
 
-export default function SubArea({gnb1,gnb2}) {
+export default function SubArea({ gnb1, gnb2 }) {
   return (
     <main className="sub-container" id="main">
-<<<<<<<<< Temporary merge branch 1
-      <SubTop/>
-      <Outlet/>
+      <Outlet />
     </main>
   );
 } //////////// MainArea 컴포넌트 ///////////
