70e602c10f6058e37e525216de5deac0779277fb
minji73 <kimminji7373@gmail.com>
2025-03-11 14:31:12 +0900

    x

--- project/src/components/page/Error.jsx
+++ project/src/components/page/Error.jsx
@@ -3,9 +3,18 @@ import { useEffect, useLayoutEffect, useState, createContext } from "react";
 
 function Error() {
   return (
-    <>
-      <h1>에러</h1>
-    </>
+    <div className="error-wrap">
+      <img src={""} alt="서비스 오류 이미지" />
+      <div className="text-wrap">
+        <h1>서비스 이용 불가</h1>
+        <p>
+          현재 서비스가 일시적으로 중단되었습니다. 잠시 후 다시 시도해 주세요.
+        </p>
+        <p>불편을 드려 죄송합니다.</p>
+        <br />
+        <a href="#">홈페이지로 돌아가기</a>
+      </div>
+    </div>
   );
 }
 
