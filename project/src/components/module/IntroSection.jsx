ca96b2d911c3a257da274b5f12a255bf856a076c
eddyhwang97 <hwangdw1997@gmail.com>
2025-03-17 09:57:10 +0900

    메인 검색하기 링크 수정

--- project/src/components/module/IntroSection.jsx
+++ project/src/components/module/IntroSection.jsx
@@ -10,7 +10,7 @@ function IntroSection() {
             당신의 취향을 발견하는 공간, <strong>칙칙북북</strong>입니다.
           </h2>
           <div className="link-box-wrap">
-            <a href="/search/totalsearch" className="link-box">
+            <a href="/totalsearch" className="link-box">
               <p>대출하기</p>
               <span className="link-box-bg bg01"></span>
             </a>
